353dd32e7ef3011d443d027b3e45fc11715e8fe0
cyc-func <yasharyang@gmail.com>
2016-12-03 10:44:22 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
 	int n = 1000;			// a + b + c = n
-  
 	n /= 2;
-	for(int r = 1; r <= n; r++)
+
+	for(int r = (int) sqrt(n / 2); r <= (int) sqrt(n); r++)
 	{
 		if(n % r == 0)
 		{
 			int s = (n / r) - r;
-			if(s > 0 && r > s)
-			{
-				printf("%d %d %d\n", r*r - s*s, 2*r*s, r*r + s*s);
-				printf("Product is %d\n", (2*r*s) * (r*r - s*s) * (r*r + s*s));
-			}
+			printf("%d %d %d\n", r*r - s*s, 2*r*s, r*r + s*s);
+			// now just find their product
 		}
 	}
 
